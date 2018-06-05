c91a50579637dd334d27215e85208487e528f435
Huy Nguyen <conghuy2012@gmail.com>
2018-06-05 17:21:39 +0700

    Add some example

---
+++ Example/EzPopup/NumberPickerViewController.swift
@@ -0,0 +1,50 @@
+//
+//  NumberPickerViewController.swift
+//  EzPopup_Example
+//
+//  Created by Huy Nguyen on 6/5/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+protocol NumberPickerViewControllerDelegate: class {
+    func numberPickerViewController(sender: NumberPickerViewController, didSelectNumber number: Int)
+}
+
+class NumberPickerViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    weak var delegate: NumberPickerViewControllerDelegate?
+    
+    static func instantiate() -> NumberPickerViewController? {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(NumberPickerViewController.self)") as? NumberPickerViewController
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "\(UITableViewCell.self)")
+        tableView.separatorInset = UIEdgeInsets.zero
+    }
+
+}
+
+extension NumberPickerViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(UITableViewCell.self)", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        delegate?.numberPickerViewController(sender: self, didSelectNumber: indexPath.row)
+    }
+}
--- Example/EzPopup/ViewController.swift
+++ Example/EzPopup/ViewController.swift
@@ -12,16 +12,40 @@ import EzPopup
 class ViewController: UIViewController {
     
     let customAlertVC = CustomAlertViewController.instantiate()
+    let pickerVC = NumberPickerViewController.instantiate()
 
-    @IBAction func testButtonTapped(_ sender: Any) {
+    @IBAction func showAtCenterButtonTapped(_ sender: Any) {
         guard let customAlertVC = customAlertVC else { return }
         
         customAlertVC.titleString = "Congratulation"
         customAlertVC.messageString = "You just showed a custom alert view controller."
         
-        let popupVC = PopupViewController(contentController: customAlertVC, position: .right(60), popupWidth: 300)
+        let popupVC = PopupViewController(contentController: customAlertVC, popupWidth: 300)
+        popupVC.cornerRadius = 10
+        present(popupVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func showTopRightButtonTapped(_ sender: Any) {
+        guard let pickerVC = pickerVC else { return }
+        
+        pickerVC.delegate = self
+        
+        let popupVC = PopupViewController(contentController: pickerVC, position: .topRight(CGPoint(x: 16, y: 40)), popupWidth: 100, popupHeight: 200)
+        popupVC.canTapOutsideToDismiss = false
         present(popupVC, animated: true, completion: nil)
     }
     
 }
 
+extension ViewController: NumberPickerViewControllerDelegate {
+    func numberPickerViewController(sender: NumberPickerViewController, didSelectNumber number: Int) {
+        dismiss(animated: true) {
+            let alertController = UIAlertController(title: "Good luck", message: "Good luck with your number \(number)", preferredStyle: .alert)
+            
+            let closeButton = UIAlertAction(title: "Close", style: .cancel)
+            alertController.addAction(closeButton)
+            
+            self.present(alertController, animated: true)
+        }
+    }
+}
