de7cd0de85368ffee077ab9739c48d902d4e2ac2
Huy Nguyen <conghuy2012@gmail.com>
2018-06-05 16:52:49 +0700

    Add shadow to pop up

--- EzPopup/Classes/PopupViewController.swift
+++ EzPopup/Classes/PopupViewController.swift
@@ -39,11 +39,14 @@ public class PopupViewController: UIViewController {
     public var canTapOutsideToDismiss = true
     
     public var cornerRadius: CGFloat = 0
+    public var shadowEnabled = true
     
     public var contentController: UIViewController?
     public var contentView: UIView?
     
     weak var delegate: PopupViewControllerDelegate?
+    
+    private var containerView = UIView()
     
     // MARK: -
     
@@ -94,43 +97,58 @@ public class PopupViewController: UIViewController {
     }
     
     private func setupUI() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
         contentView?.translatesAutoresizingMaskIntoConstraints = false
+        
         view.backgroundColor = backgroundColor.withAlphaComponent(backgroundAlpha)
+        
+        if cornerRadius > 0 {
+            contentView?.layer.cornerRadius = cornerRadius
+            contentView?.layer.masksToBounds = true
+        }
+        
+        if shadowEnabled {
+            containerView.layer.shadowOpacity = 0.5
+            containerView.layer.shadowColor = UIColor.black.cgColor
+            containerView.layer.shadowOffset = CGSize(width: 5, height: 5)
+            containerView.layer.shadowRadius = 5
+        }
     }
     
     private func setupViews() {
-        guard let contentView = contentView else {
-            print("NOTE: Please set contentView")
-            return
-        }
-        
         if let contentController = contentController {
             addChildViewController(contentController)
         }
         
-        view.addSubview(contentView)
-        
-        if cornerRadius > 0 {
-            contentView.layer.cornerRadius = cornerRadius
-            contentView.layer.masksToBounds = true
-        }
-        
+        addViews()
         addSizeConstraints()
         addPositionConstraints()
+    }
+    
+    private func addViews() {
+        view.addSubview(containerView)
+        
+        if let contentView = contentView {
+            containerView.addSubview(contentView)
+            
+            let topConstraint = NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: containerView, attribute: .top, multiplier: 1, constant: 0)
+            let leftConstraint = NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: containerView, attribute: .left, multiplier: 1, constant: 0)
+            let bottomConstraint = NSLayoutConstraint(item: contentView, attribute: .bottom, relatedBy: .equal, toItem: containerView, attribute: .bottom, multiplier: 1, constant: 0)
+            let rightConstraint = NSLayoutConstraint(item: contentView, attribute: .right, relatedBy: .equal, toItem: containerView, attribute: .right, multiplier: 1, constant: 0)
+            NSLayoutConstraint.activate([topConstraint, leftConstraint, bottomConstraint, rightConstraint])
+        }
     }
     
     // MARK: - Add constraints
     
     private func addSizeConstraints() {
-        guard let contentView = contentView else { return }
-        
         if let popupWidth = popupWidth {
-            let widthConstraint = NSLayoutConstraint(item: contentView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: popupWidth)
+            let widthConstraint = NSLayoutConstraint(item: containerView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: popupWidth)
             NSLayoutConstraint.activate([widthConstraint])
         }
         
         if let popupHeight = popupHeight {
-            let heightConstraint = NSLayoutConstraint(item: contentView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: popupHeight)
+            let heightConstraint = NSLayoutConstraint(item: containerView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: popupHeight)
             NSLayoutConstraint.activate([heightConstraint])
         }
     }
@@ -167,73 +185,56 @@ public class PopupViewController: UIViewController {
     }
     
     private func addCenterPositionConstraints(offset: CGPoint?) {
-        guard let contentView = contentView else { return }
-        let centerXConstraint = NSLayoutConstraint(item: contentView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: offset?.x ?? 0)
-        let centerYConstraint = NSLayoutConstraint(item: contentView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: offset?.y ?? 0)
+        let centerXConstraint = NSLayoutConstraint(item: containerView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: offset?.x ?? 0)
+        let centerYConstraint = NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: offset?.y ?? 0)
         NSLayoutConstraint.activate([centerXConstraint, centerYConstraint])
     }
     
     private func addTopLeftPositionConstraints(offset: CGPoint?) {
-        guard let contentView = contentView else { return }
-        
-        let topConstraint = NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: offset?.y ?? 0)
-        let leftConstraint = NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: offset?.x ?? 0)
+        let topConstraint = NSLayoutConstraint(item: containerView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: offset?.y ?? 0)
+        let leftConstraint = NSLayoutConstraint(item: containerView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: offset?.x ?? 0)
         NSLayoutConstraint.activate([topConstraint, leftConstraint])
     }
     
     private func addTopRightPositionConstraints(offset: CGPoint?) {
-        guard let contentView = contentView else { return }
-        
-        let topConstraint = NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: offset?.y ?? 0)
-        let rightConstraint = NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: contentView, attribute: .right, multiplier: 1, constant: offset?.x ?? 0)
+        let topConstraint = NSLayoutConstraint(item: containerView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: offset?.y ?? 0)
+        let rightConstraint = NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: containerView, attribute: .right, multiplier: 1, constant: offset?.x ?? 0)
         NSLayoutConstraint.activate([topConstraint, rightConstraint])
     }
     
     private func addBottomLeftPositionConstraints(offset: CGPoint?) {
-        guard let contentView = contentView else { return }
-        
-        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: offset?.y ?? 0)
-        let leftConstraint = NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: offset?.x ?? 0)
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: containerView, attribute: .bottom, multiplier: 1, constant: offset?.y ?? 0)
+        let leftConstraint = NSLayoutConstraint(item: containerView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: offset?.x ?? 0)
         NSLayoutConstraint.activate([bottomConstraint, leftConstraint])
     }
     
     private func addBottomRightPositionConstraints(offset: CGPoint?) {
-        guard let contentView = contentView else { return }
-        
-        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: offset?.y ?? 0)
-        let rightConstraint = NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: contentView, attribute: .right, multiplier: 1, constant: offset?.x ?? 0)
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: containerView, attribute: .bottom, multiplier: 1, constant: offset?.y ?? 0)
+        let rightConstraint = NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: containerView, attribute: .right, multiplier: 1, constant: offset?.x ?? 0)
         NSLayoutConstraint.activate([bottomConstraint, rightConstraint])
     }
     
     private func addTopPositionConstraints(offset: CGFloat) {
-        guard let contentView = contentView else { return }
-        
-        let topConstraint = NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: offset)
-        let centerXConstraint = NSLayoutConstraint(item: view, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1, constant: 0)
+        let topConstraint = NSLayoutConstraint(item: containerView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: offset)
+        let centerXConstraint = NSLayoutConstraint(item: view, attribute: .centerX, relatedBy: .equal, toItem: containerView, attribute: .centerX, multiplier: 1, constant: 0)
         NSLayoutConstraint.activate([topConstraint, centerXConstraint])
     }
     
     private func addLeftPositionConstraints(offset: CGFloat) {
-        guard let contentView = contentView else { return }
-        
-        let leftConstraint = NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: offset)
-        let centerYConstraint = NSLayoutConstraint(item: view, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1, constant: 0)
+        let leftConstraint = NSLayoutConstraint(item: containerView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: offset)
+        let centerYConstraint = NSLayoutConstraint(item: view, attribute: .centerY, relatedBy: .equal, toItem: containerView, attribute: .centerY, multiplier: 1, constant: 0)
         NSLayoutConstraint.activate([leftConstraint, centerYConstraint])
     }
     
     private func addBottomPositionConstraints(offset: CGFloat) {
-        guard let contentView = contentView else { return }
-        
-        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: offset)
-        let centerXConstraint = NSLayoutConstraint(item: view, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: containerView, attribute: .bottom, multiplier: 1, constant: offset)
+        let centerXConstraint = NSLayoutConstraint(item: view, attribute: .centerX, relatedBy: .equal, toItem: containerView, attribute: .centerX, multiplier: 1, constant: 0)
         NSLayoutConstraint.activate([bottomConstraint, centerXConstraint])
     }
     
     private func addRightPositionConstraints(offset: CGFloat) {
-        guard let contentView = contentView else { return }
-        
-        let rightConstraint = NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: contentView, attribute: .right, multiplier: 1, constant: offset)
-        let centerXConstraint = NSLayoutConstraint(item: view, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1, constant: 0)
+        let rightConstraint = NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: containerView, attribute: .right, multiplier: 1, constant: offset)
+        let centerXConstraint = NSLayoutConstraint(item: view, attribute: .centerY, relatedBy: .equal, toItem: containerView, attribute: .centerY, multiplier: 1, constant: 0)
         NSLayoutConstraint.activate([rightConstraint, centerXConstraint])
     }
 
@@ -249,10 +250,10 @@ public class PopupViewController: UIViewController {
 // MARK: - UIGestureRecognizerDelegate
 extension PopupViewController: UIGestureRecognizerDelegate {
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        guard let touchView = touch.view, let contentView = contentView, canTapOutsideToDismiss else {
+        guard let touchView = touch.view, canTapOutsideToDismiss else {
             return false
         }
         
-        return !touchView.isDescendant(of: contentView)
+        return !touchView.isDescendant(of: containerView)
     }
 }
