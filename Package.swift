35ec53d641bfff554860d867dfd52e98ff3afeae
Huy Nguyen <conghuy2012@gmail.com>
2021-05-24 17:50:57 +0700

    Update Package iOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EzPopup",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
